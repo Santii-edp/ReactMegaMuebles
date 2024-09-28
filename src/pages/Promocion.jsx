8dc95bc27e036410f2baf9acb706f83caf6d8f9a
Gian Cortés <gigiluicort@gmail.com>
2024-09-27 19:07:27 -0500

    actualizadooo

---
+++ src/pages/Promocion.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import NavBar from '../components/Navbar';
+
+const PromocionMuebles = () => {
+  return (
+    <>
+    <NavBar/>
+    <div style={styles.container}>
+      <h1 style={styles.header}>Muebles esenciales para tu hogar a un nuevo precio más bajo</h1>
+      <p style={styles.paragraph}>
+        ¡Esto no es solo una oferta, es una realidad! En <strong>MegaMuebles</strong> nos comprometemos a
+        ofrecer una amplia variedad de muebles a precios accesibles. Hemos reducido nuevamente los precios en cientos de
+        productos para que puedas crear espacios confortables y acogedores para ti y tus seres queridos.
+      </p>
+
+      <h2 style={styles.subHeader}>Tus espacios siempre listos para el descanso</h2>
+      <p style={styles.paragraph}>
+        El dormitorio es un refugio de paz y descanso, y la cama debe ser el foco central de este espacio. Sin embargo,
+        el confort va más allá de una buena cama. Tener el colchón perfecto, las almohadas adecuadas y la ropa de cama
+        que se adapta a tu estilo de descanso es esencial para un buen dormir. Encuentra todo lo que necesitas para
+        asegurar noches de descanso y comodidad.
+      </p>
+
+      <h2 style={styles.subHeader}>Muebles y accesorios para organizar tu hogar</h2>
+      <p style={styles.paragraph}>
+        Ofrecemos múltiples soluciones para almacenamiento y organización, aprovechando al máximo cada rincón de tu
+        hogar. Desde armarios amplios para toda tu ropa y accesorios hasta organizadores de cajones y estanterías,
+        encontrarás todo lo necesario para mantener tus espacios ordenados y funcionales.ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ
+      </p>
+    </div>
+    </>
+  );
+};
+
+// Estilos
+const styles = {
+  container: {
+    padding: '20px',
+    width: '100%', // Ocupar todo el ancho de la pantalla
+    fontFamily: 'Arial, sans-serif',
+    lineHeight: '1.6',
+    textAlign: 'left', // Asegura la alineación a la izquierda
+  },
+  header: {
+    fontSize: '36px', // Tamaño más grande para el título principal
+    fontWeight: 'bold',
+    marginBottom: '20px',
+  },
+  subHeader: {
+    fontSize: '28px', // Tamaño más grande para los subtítulos
+    fontWeight: 'bold',
+    marginTop: '20px',
+    marginBottom: '10px',
+  },
+  paragraph: {
+    fontSize: '20px', // Tamaño más grande para el texto normal
+    color: '#333',
+    marginBottom: '15px',
+  },
+};
+
+export default PromocionMuebles;
